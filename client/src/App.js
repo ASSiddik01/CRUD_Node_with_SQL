c0eaa3b8c16e1741126cbad2c6d12794c53d4fb6
Shama <assiddik001@gmail.com>
2022-06-29 20:39:24 +0600

    show data on UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
 import { useState } from "react";
+import { useQuery } from "react-query";
 
 function App() {
   const [inputEdit, setInputEdit] = useState(false);
+  const {
+    data: todos,
+    isLoading,
+    refetch,
+  } = useQuery("todos", () =>
+    fetch(`http://localhost:5000/get`).then((res) => res.json())
+  );
+
+  if (isLoading) {
+    return "Data loading";
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const taskName = e.target.taskName.value;
@@ -55,52 +68,59 @@ function App() {
           </form>
         </div>
         <div className="border rounded-lg p-2 w-3/4 text-start">
-          <div className=" text-center">Output Field</div>
+          <div className=" text-center">Output Field </div>
           <ul>
-            <li className="border-2 border-lime-400 p-2 m-2 rounded">
-              <div>
-                <div className="space-x-12">
-                  <span>Task name </span>
-                  <span className="text-orange-600">Priroity</span>{" "}
-                </div>
-                <hr className="border-t-2 border-t-lime-400 my-2" />
-                <div className="space-x-4">
-                  <form
-                    onSubmit={handleEdit}
-                    className={`${inputEdit ? "block" : "hidden"} space-x-4`}
-                  >
-                    <input
-                      type="text"
-                      placeholder="Update priroity "
-                      className="border border-gray-500 rounded"
-                      name="editPriroity"
-                      id="editPriroity"
-                    />
-                    <input
-                      type="submit"
-                      value={"Update"}
-                      className="btn btn-primary px-4 py-1 rounded bg-green-500"
-                    ></input>
-                    <button
-                      onClick={() => setInputEdit(false)}
-                      className="btn btn-primary px-4 py-1 rounded bg-red-600"
+            {todos.map((todo) => (
+              <li
+                key={todo.id}
+                className="border-2 border-lime-400 p-2 m-2 rounded"
+              >
+                <div>
+                  <div className="space-x-12">
+                    <span>{todo.task}</span>
+                    <span className="text-orange-600">
+                      {todo.priority}
+                    </span>{" "}
+                  </div>
+                  <hr className="border-t-2 border-t-lime-400 my-2" />
+                  <div className="space-x-4">
+                    <form
+                      onSubmit={handleEdit}
+                      className={`${inputEdit ? "block" : "hidden"} space-x-4`}
                     >
-                      Cancel
-                    </button>
-                  </form>
+                      <input
+                        type="text"
+                        placeholder="Update priroity "
+                        className="border border-gray-500 rounded"
+                        name="editPriroity"
+                        id="editPriroity"
+                      />
+                      <input
+                        type="submit"
+                        value={"Update"}
+                        className="btn btn-primary px-4 py-1 rounded bg-green-500"
+                      ></input>
+                      <button
+                        onClick={() => setInputEdit(false)}
+                        className="btn btn-primary px-4 py-1 rounded bg-red-600"
+                      >
+                        Cancel
+                      </button>
+                    </form>
 
-                  <button
-                    onClick={() => setInputEdit(true)}
-                    className="btn btn-primary px-4 py-1 rounded bg-green-500"
-                  >
-                    Edit
-                  </button>
-                  <button className="btn btn-primary px-4 py-1 rounded bg-red-600">
-                    Delete
-                  </button>
+                    <button
+                      onClick={() => setInputEdit(true)}
+                      className="btn btn-primary px-4 py-1 rounded bg-green-500"
+                    >
+                      Edit
+                    </button>
+                    <button className="btn btn-primary px-4 py-1 rounded bg-red-600">
+                      Delete
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </li>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
